736f923472b1908c592afe96acbe141bc8386549
Riku Rauhala <52216889+rvrauhala@users.noreply.github.com>
2021-06-09 23:16:03 +0300

    Add command and basic functionality for counting tj

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import discord, dotenv, os
+from datetime import date
 from discord.ext import commands
 
 dotenv.load_dotenv()
@@ -13,5 +14,13 @@ async def on_ready():
 @bot.command(name="aamuja", help="Huutelee aamuja")
 async def aamuja(ctx):
     await ctx.send("Aamuja!")
+
+@bot.command(name="tj", help="Tänään jäljellä")
+async def tj(ctx):
+    today = date.today()
+    # Saapumiserä I/21
+    tj0 = date(2021, 12, 16)
+    tj = abs((today-tj0).days)
+    await ctx.send(f"Tänään jäljellä: **{tj}** aamua")
 
 bot.run(TOKEN)
