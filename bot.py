f43401ebbcbaedb9e408b5a7da18e4df26de11d7
Riku Rauhala <52216889+rvrauhala@users.noreply.github.com>
2021-06-10 22:06:39 +0300

    Add detail function and refactor counting tj

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import discord, dotenv, os
-from datetime import date
+from datetime import date, datetime
 from discord.ext import commands
 
 dotenv.load_dotenv()
@@ -17,10 +17,32 @@ async def aamuja(ctx):
 
 @bot.command(name="tj", help="Tänään jäljellä")
 async def tj(ctx):
+    tj = count_tj()
+    await ctx.send(f"Tänään jäljellä: **{tj}** aamua")
+
+@bot.command(name="lisätietoja", help="Lisätietoja")
+async def lisatietoja(ctx):
+    se = "I/21"
+    pa = 347
+    tj0 = datetime(2021, 12, 16)
+    now = datetime.now()
+    days = count_tj()
+    seconds = abs(now-tj0).total_seconds()
+    hours = seconds//3600
+    minutes = seconds//60
+    details = f"```Saapumiserä: {se} \n" \
+              f"Palvelusaika: {pa} \n\n" \
+              f"TÄNÄÄN JÄLJELLÄ \n" \
+              f"Päivinä: {days} \n" \
+              f"Tunteina: {hours:.0f} \n" \
+              f"Minuutteina: {minutes:.0f} \n" \
+              f"Sekunteina: {seconds:.0f}```"
+    await ctx.send(details)
+
+def count_tj():
     today = date.today()
-    # Saapumiserä I/21
     tj0 = date(2021, 12, 16)
     tj = abs((today-tj0).days)
-    await ctx.send(f"Tänään jäljellä: **{tj}** aamua")
+    return tj
 
 bot.run(TOKEN)
