2f44869ef14d1ae49a076ab6d23a58911ae765cf
Riku Rauhala <52216889+rikurauhala@users.noreply.github.com>
2021-08-12 00:09:29 +0300

    Add a new cog for basic functionality

---
+++ application/cogs/toiminnot.py
@@ -0,0 +1,73 @@
+from datetime import datetime
+
+from pytz import timezone
+
+from discord.ext import commands
+
+from cogs.functions.tj import count_tj, get_tj0
+from cogs.functions.validation import valid
+
+timezone = timezone("Europe/Helsinki")
+
+default_contingent = "2/21"
+default_duration = 347
+
+class Toiminnot(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="tj", help="Tänään jäljellä")
+    async def tj(self, ctx, contingent: str = default_contingent, duration: int = default_duration):
+        if not valid(ctx, contingent, duration):
+            return
+        tj = count_tj(contingent, duration)
+        if tj > 0:
+            await ctx.send(f"Tänään jäljellä: **{tj}** aamua")
+        else:
+            await ctx.send(f"Tänään jäljellä: **{tj}** aamua, ohi on!")
+
+    @commands.command(name="ohi", help="Aamuja ohi")
+    async def ohi(self, ctx, contingent: str = default_contingent, duration: int = default_duration):
+        if not valid(ctx, contingent, duration):
+            return
+        tj = count_tj(contingent, duration)
+        ohi = 347-tj
+        await ctx.send(f"Ohi on: **{ohi}** aamua")
+
+    @commands.command(name="lisätietoja", help="Yksityiskohtaisempaa tietoa")
+    async def lisatietoja(self, ctx, contingent: str = default_contingent, duration: int = default_duration):
+        if not valid(ctx, contingent, duration):
+            return
+        tj0 = get_tj0(contingent, duration)
+        tj = count_tj(contingent, duration)
+        if tj < 0:
+            await ctx.send("Ohi on!")
+            return
+        now = datetime.now(tz=timezone)
+        seconds = abs(now-tj0).total_seconds()
+        minutes = seconds/60
+        hours = minutes/60
+        days = hours/24
+        weeks = days/7
+        months = days/30.437
+        years = days/365
+        ohi = duration-tj
+        percent = 100*(1-days/duration)
+        details = f"```Saapumiserä: {contingent} \n" \
+                f"Palvelusaika: {duration} \n\n" \
+                f"TÄNÄÄN JÄLJELLÄ \n" \
+                f"- Vuosina: {years:.2f} \n" \
+                f"- Kuukausina: {months:.2f} \n" \
+                f"- Viikkoina: {weeks:.1f} \n" \
+                f"- Päivinä: {days:.1f} \n" \
+                f"- Tunteina: {hours:.0f} \n" \
+                f"- Minuutteina: {minutes:.0f} \n" \
+                f"- Sekunteina: {seconds:.0f} \n\n" \
+                f"OHI ON \n" \
+                f"- {ohi} aamua ({percent:.2f} %)```"
+        await ctx.send(details)
+
+
+def setup(bot):
+    bot.add_cog(Toiminnot(bot))
