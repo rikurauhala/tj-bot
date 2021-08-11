f8c33b12957e1a1cc83051809fbc199574497ee8
Riku Rauhala <52216889+rikurauhala@users.noreply.github.com>
2021-08-12 00:08:51 +0300

    Remove unused imports

--- application/bot.py
+++ application/bot.py
@@ -6,13 +6,8 @@ from pytz import timezone
 
 from discord.ext import commands
 
-from tj import count_tj, get_tj0
-from validation import valid
-
 dotenv.load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
-
-timezone = timezone("Europe/Helsinki")
 
 description = "tj-bot kertoo montako aamua on tänään jäljellä"
 bot = commands.Bot(command_prefix="!", description=description)
