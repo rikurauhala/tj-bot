70a7167971516493bf94422964bf4ed1017987a3
Riku Rauhala <52216889+rikurauhala@users.noreply.github.com>
2021-08-13 23:14:27 +0300

    Print working directory for debugging purposes

--- application/bot.py
+++ application/bot.py
@@ -12,6 +12,7 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 description = "tj-bot kertoo montako aamua on tänään jäljellä"
 bot = commands.Bot(command_prefix="!", description=description)
 
+print(os.getcwd())
 for file in os.listdir("./cogs"):
     if file.endswith(".py"):
         cog = file[:-3]
